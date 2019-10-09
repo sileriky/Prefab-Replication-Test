03bf21f3eb1b0c0a44a67ea762b670d716da4402
Eric Vasile <32675397+sileriky@users.noreply.github.com>
2019-10-09 10:04:04 +0100

    Staircase v0.1
    Staircase works for the most part.
    
    ISSUE: As a result of using an initial tile for working out the size of a tile, there's an extra step at the very top.

---
+++ Assets/Scripts/WindowTileSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowTileSpawner : MonoBehaviour {
+
+    //Ref to window tile prefab file
+    //Requires link in editor!!!
+    public GameObject WindowTilePrefab;
+
+    public int staircaseHeight;
+
+    // Use this for initialization
+    void Start () {
+
+        //Get position of spawner
+        float selfX = transform.position.x;
+        float selfY = transform.position.y;
+
+        //Setup initial tile
+        Vector3 initPos = transform.position;
+        GameObject newWindow = Instantiate(WindowTilePrefab, initPos, Quaternion.identity);
+
+        //Get dimensions of prefab tile
+        Vector3 prefabDims = newWindow.GetComponent<Renderer>().bounds.size;
+
+        for (int curStep = 1; curStep <= staircaseHeight; curStep++) {
+            for (int curStepTile = 0; curStepTile < curStep; curStepTile++) {
+                Vector3 curPos = new Vector3(curStepTile * prefabDims.x + selfX, -curStep * prefabDims.y + selfY, 0);
+                Instantiate(WindowTilePrefab, curPos, Quaternion.identity);
+            }
+            
+        }
+
+    }
+	
+}
